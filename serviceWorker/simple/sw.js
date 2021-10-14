eb3c69fc19139d0059a27d20a6d229ff003c2961
chijing.zt <chijing.zt@alibaba-inc.com>
2021-10-14 20:39:38 +0800

    finish worker demo

--- serviceWorker/simple/sw.js
+++ serviceWorker/simple/sw.js
@@ -1,8 +1,10 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
+let CACHE_NAME = 'my-site-cache-v1';
+let urlsToCache = [
   '/simple/baidu.png',
-  '/simple/cuowu.jpeg'
+  '/simple/cuowu.jpeg',
+  '/hello'
 ];
+let m = true;
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -12,15 +14,33 @@ self.addEventListener('install', (event) => {
         return cache.addAll(urlsToCache);
       })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
   console.log('fetch', event.request.url);
+
+  // 仅使用Cache（Cache only）
+  // event.respondWith(caches.match(event.request));
+
+  // 仅使用网络（Network only）
+  // event.respondWith(fetch(event.request));
+
+  // 先使用 SW 缓存，没有则使用网络资源
+  // event.respondWith(
+  //   caches.match(event.request).then(function (response) {
+  //     return response || fetch(event.request);
+  //   })
+  // );
+
+  // 正常模式
   if (event.request.url.match(/.html$/)) {
-    fetch(event.request)
-      .catch(function () {
-        return caches.match('/simple/cuowu.jpeg');
-      });
+    event.respondWith(
+      fetch(event.request)
+        .catch(function () {
+          return caches.match('/simple/cuowu.jpeg');
+        })
+    );
   } else {
     event.respondWith(
       caches.match(event.request).then(function (resp) {
@@ -41,3 +61,10 @@ self.addEventListener('activate', () => {
   console.log('activate');
 });
 
+self.onmessage = (ev) => {
+  if (ev.data.edit === 'edit') {
+    m = !m;
+  } else {
+    console.log('m->>>>', m);
+  }
+};
